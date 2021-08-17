7939bca9190a2f1a469661abd2c4b582455a6f8d
joacasallas <joacasallas@gmail.com>
2021-08-17 16:02:53 -0500

    shell.h

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <wait.h>
+#include <sys/types.h>
+
+int gestStringLength(char *string);
+
+#endif
