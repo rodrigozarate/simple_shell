ab73cf99968a547cba3944c53ca5956db0fef775
joacasallas <joacasallas@gmail.com>
2021-08-17 14:24:31 -0500

    shellFunctions.c

---
+++ shellFunctions.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <wait.h>
+#include <sys/types.h>
+#include "shell.h"
+
+int countPipes(char text[]){ /*verificamos si hay pipes*/
+	if(strchr(text, '|')!=NULL){
+		return (1);
+	}
+	return (0);
+}
+
+
+int countRedirection(char text[]){ /*verificamos si hay simbolo de redireccion*/
+	if(strchr(text, '>')!=NULL){
+		return (1);
+	}
+	return (0);
+}
+
+void split(char text[], char *splittedtext[], char DELIM[]){ /*dividimos el texto en 2: en antes de .. y en despues de...*/
+	char *tokenpointer;
+	int i = 0;
+	tokenpointer = strtok(text, DELIM);
+
+	while(tokenpointer != NULL){
+		splittedtext[i] = tokenpointer;
+		tokenpointer = strtok(NULL, DELIM);
+		i++;
+	}
+}
