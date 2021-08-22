0651d5d1f4d4bdfab57ffa948cfed1b0432ff738
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-21 23:46:09 -0500

    [CREATE]

---
+++ builtins.c
@@ -0,0 +1,14 @@
+/**
+* builtins.c - List of buitins and functions
+* Author: Rodrigo Zárate Algecira and Joana Casallas
+* Date: August 21, 2021
+*/
+
+/* list of buid ins */
+
+, ., [, alias, bg, bind, break, builtin,
+case, cd, command, compgen, complete, continue, declare, dirs, disown, echo, enable, eval,
+exec,  exit,  export,  fc,  fg,  getopts, hash, help, history, if, jobs, kill, let, local,
+logout, popd, printf, pushd, pwd, read,  readonly,  return,  set,  shift,  shopt,  source,
+suspend,  test,  times,  trap,  type, typeset, ulimit, umask, unalias, unset, until, wait,
+while.
