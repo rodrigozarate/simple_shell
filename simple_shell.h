af01805af5b69a32dd0a1c9563437c2cd171d681
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-22 15:17:50 -0500

    [UPDATE] Add headers

--- simple_shell.h
+++ simple_shell.h
@@ -1,7 +1,14 @@
 #ifndef SIMPLE_SHELL_H
 #define SIMPLE_SHELL_H
 
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <sys/types.h>
+
 char *read_input(void);
 char **break_input(char *input);
+int rocket(char **args);
 
 #endif /* SIMPLE_SHELL_H */
