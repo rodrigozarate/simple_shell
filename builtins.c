be0288e91f6fac762618ad86b049d0adf0230ecf
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-22 15:51:14 -0500

    [UPDATE] Skeleton of function

--- builtins.c
+++ builtins.c
@@ -1,14 +1,25 @@
-/**
+/*
 * builtins.c - List of buitins and functions
 * Author: Rodrigo Zárate Algecira and Joana Casallas
-* Date: August 21, 2021
+* Date: August 22, 2021
 */
 
-/* list of buid ins */
+#include "simple_shell.h"
 
+/**
+* builtins - List of builtins
+* Return: int
+*/
+
+int builtins()
+{
+char *builtinlist[] = {
 , ., [, alias, bg, bind, break, builtin,
 case, cd, command, compgen, complete, continue, declare, dirs, disown, echo, enable, eval,
 exec,  exit,  export,  fc,  fg,  getopts, hash, help, history, if, jobs, kill, let, local,
 logout, popd, printf, pushd, pwd, read,  readonly,  return,  set,  shift,  shopt,  source,
 suspend,  test,  times,  trap,  type, typeset, ulimit, umask, unalias, unset, until, wait,
 while.
+}
+	return (sizeof(builtin_str) / sizeof(char *));
+}
