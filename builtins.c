b201e157005482a1e385bdeda096e8ede323baed
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-22 18:51:37 -0500

    [UPDATE]

--- builtins.c
+++ builtins.c
@@ -13,13 +13,7 @@
 
 int builtins()
 {
-char *builtinlist[] = {
-, ., [, alias, bg, bind, break, builtin,
-case, cd, command, compgen, complete, continue, declare, dirs, disown, echo, enable, eval,
-exec,  exit,  export,  fc,  fg,  getopts, hash, help, history, if, jobs, kill, let, local,
-logout, popd, printf, pushd, pwd, read,  readonly,  return,  set,  shift,  shopt,  source,
-suspend,  test,  times,  trap,  type, typeset, ulimit, umask, unalias, unset, until, wait,
-while.
-}
-	return (sizeof(builtin_str) / sizeof(char *));
+char *builtinlist[] = {"env", "exit", "help"}
+
+	return (sizeof(builtinlist) / sizeof(char *));
 }
