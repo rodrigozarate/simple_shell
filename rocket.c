e176689f5ea83473c8eb798d977eebd0cb3650d4
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-24 01:57:04 -0500

    [UPDATE]

--- rocket.c
+++ rocket.c
@@ -18,31 +18,25 @@ pid_t pid, w_pid;
 int state;
 char **env = environ;
 
-printf("inside rocket-0\n");
 pid = fork();
 	if (pid == 0)
 	{
-		printf("inside rocket-1\n");
 		/* its child */
 		if (execve(args[0], args, env) == -1)
 		{
-			printf("inside rocket-2\n");
 			perror("No argument");
 		}
 		exit(EXIT_FAILURE);
 	}
 	else if (pid < 0)
 	{
-		printf("inside rocket-3\n");
 		/* forking failed */
 		perror("Fork failed");
 	}
 	else
 	{
-		printf("inside rocket-4\n");
 		/* Parent process */
 		do {
-			printf("inside rocket-5\n");
 			w_pid = waitpid(pid, &state, WUNTRACED);
 		} while (!WIFEXITED(state) && !WIFSIGNALED(state));
 	}
