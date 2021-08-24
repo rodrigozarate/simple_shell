9a1964f8be9848781a1e88ce381dd4ed5c2ce88d
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-24 14:53:13 -0500

    [UPDATE]

--- rocket.c
+++ rocket.c
@@ -18,9 +18,10 @@ pid_t pid, w_pid;
 int state;
 char **env = environ;
 
-pid = fork();
+	pid = fork();
 	if (pid == 0)
 	{
+		/* on path args should be changed */
 		/* its child */
 		if (execve(args[0], args, env) == -1)
 		{
