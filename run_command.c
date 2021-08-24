352cf610fac9d67ce4d8e8882ef69d7f33455ada
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-24 02:03:41 -0500

    [UPDATE] Replace strcmp for custom function

--- run_command.c
+++ run_command.c
@@ -19,28 +19,21 @@ int i;
 
 int (*builtinfunc[])(char **) = {&bi_env, &bi_exit};
 
-printf("inside run_command-0\n");
 	if (args[0] == NULL)
 	{
-		printf("inside run_command-1 arg = 0\n");
 		/* Null error */
 		return (1);
 	}
 	/* walk array of builtins */
 	for (i = 0; i <= builtins(builtinlist); i++)
 	{
-		printf("inside run_command-2\n");
 		/* 0, if the s1 and s2 are equal */
-		if (strcmp(args[0], builtinlist[i]) == 0)
+		if (str_comp(args[0], builtinlist[i]) == 0)
 		{
-			printf("inside run_command-3\n");
-			printf("funn %s\n", builtinlist[i]);
 			/* set the corresponding function */
 			return ((*builtinfunc[i])(args));
 		}
 	}
 /* ignition secuence started */
-printf("to rok\n");
 return (rocket(args));
-
 }
