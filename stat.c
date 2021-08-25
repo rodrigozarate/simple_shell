ecd64aec6f6541a36a4e0a0fccf69570a9a75fc1
joacasallas <joacasallas@gmail.com>
2021-08-25 14:14:41 -0500

    update

--- stat.c
+++ stat.c
@@ -10,7 +10,7 @@
 int statFunction(char **args, char **stringsPath)
 {
 	struct stat statbuf;
-	int pid, execute, buffer;
+	int pid;
 
 	if (stat(stringsPath[0], &statbuf) == -1)
 	{
@@ -27,14 +27,12 @@ int statFunction(char **args, char **stringsPath)
 		}
 		if (pid == 0)
 		{
-			execute = execve(buffer, args, environ);
-			if (execute == -1)
-				exit(98);
+			rocket(args);
 		}
 	}
 	else
 	{
 		perror("not found");
-		return (0);
 	}
+	return (0);
 }
