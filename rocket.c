c8297bf0605f163321c940fa913e9d4c0a37e638
joacasallas <joacasallas@gmail.com>
2021-08-26 14:32:12 -0500

    update

--- rocket.c
+++ rocket.c
@@ -14,20 +14,15 @@
 
 int rocket(char **args)
 {
-pid_t pid, w_pid;
-int state;
-char **env = environ;
-int here = 0;
-char concat;
-int active;
-struct stat statbuf; //error al compilar porque espacio desconocido
-char *variablePath = getVariableFromEnviron("PATH");
-char **pathdivided = tokenize(variablePath);
-char concat_temp;
-
-int i = 0;
-int j = 0;
-int x = 0;
+	pid_t pid;
+	char concat;
+	struct stat statbuf; //error al compilar porque espacio desconocido
+	char *variablePath = getVariableFromEnviron("PATH");
+	char **pathdivided = tokenize(variablePath);
+	char concat_temp;
+	int i = 0;
+	int j = 0;
+	int x = 0;
 
 for (i = 0; concat_temp[i] != NULL; i++)
 {
@@ -42,44 +37,26 @@ for (i = 0; concat_temp[i] != NULL; i++)
 	}
 }
 
-i = 0;
-j = 0;
-
-active = statFunction(**pathdivided[i], concat[j]);
-	if (active == -1)
+if (stat(concat, &statbuf) == 0)
 {
-		perror("Error");
-}
-	else
+	pid = fork();
+	if (pid == -1)
 	{
-pid = fork();
+		perror("Error:");
+		return (1);
+	}
 	if (pid == 0)
 	{
-		/* on path args should be changed */
-		/* its child */
-		if (execve(concat[0], args, env) == -1)
-		{
-			perror("Error");
-		}
-		exit(EXIT_FAILURE);
-	}
-	else if (pid < 0)
-	{
-		/* forking failed */
-		perror("Fork failed");
+		execute = execve(concat, args, environ);
+		if (execute == -1)
+			exit(98);
 	}
 	else
 	{
-		/* Parent process */
-		do {
-			w_pid = waitpid(pid, &state, WUNTRACED);
-			if (w_pid == 0)
-			{
-				here++;
-			}
-		} while (!WIFEXITED(state) && !WIFSIGNALED(state));
+		wait(&status);
+		break;
 	}
-	}
-	/* always 1 */
-return (1);
+}
+}
+return (0);
 }
